560ff25f480d163a047cb8f34a623a3fa4158105
mohamad vahedi <mohamad.vahedi@admins-MacBook-Air-7.local>
2025-02-01 21:38:46 +0330

    feat: add outstatic to my site

---
+++ app/(cms)/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type RootLayoutProps = Readonly<{
+  children: React.ReactNode;
+}>;
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
